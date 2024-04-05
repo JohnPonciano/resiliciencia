34bdf769566901d6fa029d59e9903aed6fc65d26
jponciano-medens <jonathan.ponciano@medens.com>
2024-04-05 13:26:12 -0300

    add music bg in cat and plant

--- cat/script.js
+++ cat/script.js
@@ -1,14 +1,27 @@
 const catImg = document.getElementById('cat');
 const message = document.getElementById('message');
 
+// Adiciona a trilha sonora de fundo
+const backgroundAudio = new Audio('trilha.mp3');
+backgroundAudio.loop = true;
+backgroundAudio.play();
+
+// Adiciona o evento de clique na imagem do gato
 catImg.addEventListener('click', () => {
+  // Altera a imagem do gato e inicia a animação
   catImg.src = 'sleeping-cat.gif';
   catImg.style.animation = 'fall 2s ease forwards';
-  message.textContent = 'Os Gatos pois eles sempre caem de pé!! ';
-  
 
+  // Adiciona o efeito sonoro de "mew"
+  const mewSound = new Audio('mew.mp3');
+  mewSound.play();
+
+  // Atualiza a mensagem
+  message.textContent = 'Os Gatos pois eles sempre caem de pé!!';
+
+  // Define um tempo limite para a próxima etapa
   setTimeout(() => {
-    
+    // Move a imagem do gato para fora da tela
     catImg.style.transform = 'translate(-100%, 0)';
     catImg.src = 'wakeup-cat.gif';
   }, 1000);
--- plant/script.js
+++ plant/script.js
@@ -6,3 +6,16 @@ onload = () => {
     }, 1000);
   };
   
+  window.onload = () => {
+    // Inicializa a trilha sonora de fundo
+    const backgroundAudio = new Audio('planta.mp3');
+    backgroundAudio.loop = true;
+    backgroundAudio.volume = 0.5; // Ajuste o volume conforme necessário
+    backgroundAudio.play();
+  
+    // Remove a classe "not-loaded" após um atraso de 1 segundo
+    setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+    }, 1000);
+  };
+  
