a0ae5b8fd8d01791a904937eca70be2b4748ecc9
JohnPonciano <jhow.ponciano@hotmail.com>
2024-05-24 16:37:38 -0300

    add new citations

--- lion/script.js
+++ lion/script.js
@@ -1,39 +1,17 @@
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857", "#9ACD32", "#CDC673", "#CDCDC1", "#00C78C", "#D02090"];
 var quotes = [
-    ["De todos os bares de gim em todas as cidades de todo o mundo, ela entra no meu.", "Casablanca"],
-    ["Vou fazer uma oferta que ele não poderá recusar.", "O Poderoso Chefão"],
-    ["Não apenas fique parado e finja, comande e conquiste.", "Saif Ali"],
-    ["Inspire. Não motive.", "Saif Ali"],
-    ["Nunca subestime seu inimigo.", "Sun Tzu"],
-    ["É melhor manter a boca fechada e dar a impressão de que você é estúpido do que abri-la e remover todas as dúvidas.", "Rami Belson"],
-    ["A maioria das pessoas é tão feliz quanto decide ser.", "Abraham Lincoln"],
-    ["Nunca interrompa seu inimigo quando ele está cometendo um erro.", "Napoleão Bonaparte"],
-    ["Você consegue mais do que quer com uma palavra gentil e uma arma do que apenas com uma palavra gentil.", "Al Capone"],
-    ["Escolha um trabalho que você goste e nunca terá que trabalhar um dia na sua vida.", "Confúcio"],
-    ["Todos os trabalhos pagos absorvem e degradam a mente.", "Aristóteles"],
-    ["A felicidade entra sorrateiramente por uma porta que você não sabia que deixou aberta.", "John Barrymore"],
-    ["A vida é muito curta para estar solteiro.", "Saif Ali"],
-    ["A resiliência tem seu tempo.", "Saif Ali"],
-    ["A guerra só parece legal, mas tem gosto de sangue.", "Saif Ali"],
-    ["Trabalhe duro até que seus ídolos se tornem seus rivais.", "Anônimo"],
-    ["Fracasso é uma palavra desconhecida para mim.", "Jinnah"],
-    ["Não deixe o hoje passar ou o amanhã chegar, a menos que e até que você tenha alcançado as metas de hoje e definido as metas de amanhã.", "Saif Ali"],
-    ["Seu brilho é sua absorção.", "Saif Ali"],
-    ["Não deixe uma marca, deixe uma impressão.", "Saif Ali"],
-    ["O maior erro da humanidade - A Internet.", "Saif Ali"],
-    ["Talvez haja cem razões para não te amar, mas há mil razões para não te odiar.", "Saif Ali"],
-    ["Às vezes precisamos de palavras para explicar algo, usamos citações, mas quando precisamos de fatos citamos o Alcorão Sagrado.", "Saif Ali"],
-    ["Grama - Morre em um dia, decompõe-se em uma semana, renasce em um mês.", "Saif Ali"],
-    ["Não lamente. Tudo que você perde volta em outra forma.", "Rumi"],
-    ["A ferida é o lugar onde a Luz entra em você.", "Rumi"],
-    ["Os amantes não se encontram finalmente em algum lugar. Eles estão um no outro o tempo todo.", "Rumi"],
-    ["Sua tarefa não é procurar pelo amor, mas apenas buscar e encontrar todas as barreiras dentro de você que você construiu contra ele.", "Rumi"],
-    ["Quando você faz coisas com sua alma, você sente um rio se movendo em você, uma alegria.", "Rumi"],
-    ["Por que você fica na prisão quando a porta está tão aberta?", "Rumi"],
-    ["Isso é amor: voar em direção a um céu secreto, fazer cair cem véus a cada momento. Primeiro deixar a vida. Finalmente, dar um passo sem pés.", "Rumi"],
-    ["Venda sua sagacidade e compre perplexidade.", "Rumi"],
-    ["Todo mundo foi feito para algum trabalho específico, e o desejo por esse trabalho foi colocado em cada coração.", "Rumi"],
-    ["Deixe o amante ser desprezível, louco, distraído. Alguém sóbrio se preocupará com os eventos que dão errado. Deixe o amante ser.", "Rumi"]
+
+["A resiliência não é apenas aceitar o novo, mas também adaptar-se a ele.", "Autor Desconhecid"],
+["A adversidade revela o verdadeiro caráter de uma pessoa, e a resiliência molda o seu destino.", "Karen Kingsbury"],
+["A resiliência é a capacidade de encontrar alegria nas adversidades.", "Karen Reivich"],
+["Na estrada da vida, a resiliência é o combustível que nos mantém avançando.", "Autor Desconhecido"],
+["A resiliência é a virtude que nos permite saltar de volta quando a vida nos derruba.", "Autor Desconhecido"],
+["Quando enfrentamos desafios, nossa resiliência determina nossa capacidade de superá-los.", "Autor Desconhecido"],
+["A resiliência é como um músculo: quanto mais a exercitamos, mais forte ela se torna.", "Autor Desconhecido"],
+["A resiliência é a arte de manter o equilíbrio emocional em meio ao caos.", "Autor Desconhecid"],
+["Nos momentos mais difíceis, a resiliência nos lembra que somos mais fortes do que imaginamos.", "Autor Desconhecido"],
+["A resiliência não é apenas sobre sobreviver; é sobre florescer diante da adversidade.", "Karen Louise"]
+
 
 ];
 
